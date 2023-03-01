ade87675f81abff44ac268ad1dbdbba5f308b077
Karthik <kmkarthik97@gmail.com>
2023-03-01 12:02:41 +0530

    k

---
+++ chat/src/App.js
@@ -0,0 +1,131 @@
+import logo from './logo.svg';
+import './App.css';
+//import Axios from 'axios';
+import { useState, useEffect} from 'react';
+
+
+function App() {
+
+const [msgReg, setmsgReg] = useState([{}])
+const [resReg, setresReg] = useState('')
+
+// useEffect(() => {
+//   fetch("/members").then(
+//     res => res.json()
+//   ).then(
+//       msgReg => {
+//         setmsgReg(msgReg)
+//         console.log(msgReg)
+//       }
+//   )
+// }, [])
+
+  /*const date = new Date();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  const [time,setTime] = useState(`${hours};${seconds}`);
+  const [dateTime, setDateTime] = useState(`${days[day]}, ${months[month]} ${year}`);
+
+  const checkStatus = (e) => {
+    let isActive = true;
+    if(dateTime === 'Thursday, May 20 2022'){
+      isActive = false;
+    }
+    const status = document.querySelector('.status');
+    if(isActive === true){
+      status.innerHTML = 'Active';
+      status.style.color = 'green';
+    }else{
+      status.innerHTML = 'Not Active';
+      status.style.color = 'red';
+    }
+  }*/
+
+  // useEffect(() =>{
+  //   Axios.get("http://localhost:3001/api/get").then((response)=> {
+  //     setresReg(response.data);
+  //   });
+  // },[]);
+
+  // const submit = () =>{
+  //   Axios.post("http://localhost:3001/api/get", {
+  //     msg: msgReg,
+  //   }).then(() =>{
+  //     alert("succesfull")
+  //   })
+  // }
+  const fetchData = async(msg) => {
+    console.log("poda sunni")
+    console.log(msg)
+    const response = await fetch(`http://localhost:5000/${msg}`)
+    const data = await response.json();
+    console.log(data)
+    return data['message']
+  };
+
+  const handleInput = () => {
+    const botMessage = document.querySelector('#message1');
+    const userMessage = document.querySelector('#message2');
+    const inpt = document.getElementById("input").value
+    
+    
+    
+    let badwords = ['fuck|bad|stupid|useless|bitch|cringe'];
+    let words = new RegExp(badwords);
+    if(words.test(document.querySelector('#input').value)){
+      botMessage.innerHTML = 'Typing...';
+      setTimeout(() => {
+        botMessage.innerHTML = resReg.map((val) =>{ 
+          return val.res;
+        });
+        document.querySelector('#input').value = " ";
+      }, 2000);
+    }
+    userMessage.innerHTML = document.querySelector('#input').value;
+    console.log(fetchData(inpt))
+    botMessage.innerHTML = fetchData(inpt)
+  }
+  return (
+    <div className="App"> 
+      <div className="wrapper">
+        <div className="content">
+          <div className="header">
+            <div className='img'>
+              <img src={logo} alt="" />
+            </div>
+            <div className="right">
+              <div className="name">ChatBot</div>
+              <div className='status'>Active</div>
+            </div>          
+            </div>
+            <div className="main">
+              <div className='main_content'>
+                <div className='messages'>
+                  <div className='bot-message' id='message1'></div>
+                  <div className='human-message' id='message2'></div>
+                </div>
+              </div>
+            </div>
+            <div className='bottom'>
+              <div className='btm'>
+                <div className='input'>
+                  <input type="text" id="input" placeholder="Enter your message"></input>
+                </div>
+                <div className='btn'>
+                    <button onClick={handleInput}><i className='fas fa-paper-plane'>Send</i></button>
+                  </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ flask-server/server.py
@@ -0,0 +1,32 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/members/<id>")
+def members(id):
+    sentence = id
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: I do not understand...")
+    return {"members": ["Member1","Member2","Member3"]}
+
+if __name__=="__main__":
+    app.run(debug=True)
